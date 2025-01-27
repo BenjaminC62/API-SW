fd62ef161c242bfed6a8b1e59a0e5431ecfaa9f6
BenjaminC62 <benjamincornet62130@gmail.com>
2025-01-27 20:44:26 +0100

    :memo: - récuperation des données de la page web pour les stockées dans un tableau

--- app.py
+++ app.py
@@ -1,12 +1,36 @@
-from flask import Flask
+import requests
+from bs4 import BeautifulSoup
 
-app = Flask(__name__)
+BASE_URL = "https://summonerswar.fandom.com"
 
+URL = "https://summonerswar.fandom.com/wiki/Monster_Collection#Fire"
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+# La requete pour scrapper la page
+response = requests.get(URL)
+soup = BeautifulSoup(response.content, 'html.parser')
 
+monsters = []
 
-if __name__ == '__main__':
-    app.run()
+# Je récupère les informations des monstres
+for row in soup.select('table tr'):
+    link_tag = row.find('a')
+    img_tag = row.find('img')
+
+    # Si les balises sont trouvées, je récupère les informations
+    if link_tag and img_tag:
+        name = link_tag['title']
+        details_url = BASE_URL + link_tag['href']
+        img_url = img_tag['src']
+
+        monsters.append({
+            "name": name,
+            "details_url": details_url,
+            "image_url": img_url
+        })
+
+# Afficher les résultats
+for monster in monsters:
+    print(monster)
+
+print(len(monsters))
+
